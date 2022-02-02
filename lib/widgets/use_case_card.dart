5df7331c62281aebd91b1ba18e49ebc4c8b92dd6
Pedro Belfort <pedro.belfort@lutick.com>
2022-02-02 21:31:18 +0100

    Create use_case_card.dart

---
+++ lib/widgets/use_case_card.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class UseCaseCard extends StatelessWidget {
+  const UseCaseCard({
+    required this.title,
+    this.onPressed,
+    Key? key,
+  }) : super(key: key);
+
+  final String title;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints.tight(
+        const Size.square(100),
+      ),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(16.0)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black45,
+            offset: Offset(0.0, 6.0),
+            blurRadius: 12.0,
+          ),
+        ],
+      ),
+      child: TextButton(
+        onPressed: onPressed,
+        child: Text(title),
+      ),
+    );
+  }
+}
